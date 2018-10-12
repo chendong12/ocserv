1789a9877fbf15d5ef7d50327a813848277223a8
chendong12 <43974374+chendong12@users.noreply.github.com>
2018-10-12 21:17:57 +0800

    Update user_del.sh

--- user_del.sh
+++ user_del.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #ocserv 删除用户及注销用户的证书的脚本文件
 function user_del() {
-    read -p "请输入要删除的用户名称！(英文格式):" user_name
+    read -p "Please enter the name of the user you want to delete !" user_name
     if  [ ! -n "$user_name" ] ;then
-    echo "您没有输入用户名，请重新执行程序"
+    echo "You did not enter a username, please re-execute the program"
     else
     /usr/bin/ocpasswd -d $user_name
-    echo "$user_name 用户删除成功"
+    echo "$user_name User deleted successfully"
     cat /root/anyconnect/$user_name/$user_name-cert.pem >> /root/anyconnect/revoked.pem
     certtool --generate-crl --load-ca-privkey ca-key.pem  --load-ca-certificate ca-cert.pem --load-certificate revoked.pem  --template crl.tmpl --outfile crl.pem
-    echo "$user_name 用户证书被注销"
+    echo "$user_name User certificate is revoked"
     service ocserv restart
 	fi
 }
