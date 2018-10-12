1b301504b6f47934fe497bdbb8b93e7e8aa5b5c7
chendong12 <43974374+chendong12@users.noreply.github.com>
2018-10-12 11:25:48 +0800

    Create user_del.sh

---
+++ user_del.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#ocserv 删除用户及注销用户的证书的脚本文件
+function input_user() {
+	read -p "请输入要删除的用户名称！(英文格式):" user_name
+	if  [ ! -n "$user_name" ] ;then
+    echo "您没有输入用户名，请重新执行程序"
+    else
+    ocpasswd -d $user_name
+    echo "$user_name 用户删除成功"
+    cat /root/anyconnect/$user_name/$user_name-cert.pem >> /root/anyconnect/revoked.pem
+    certtool --generate-crl --load-ca-privkey ca-key.pem  --load-ca-certificate ca-cert.pem --load-certificate revoked.pem  --template crl.tmpl --outfile crl.pem
+    echo "$user_name 用户证书被注销"
+    service ocserv restart
+	fi
+}
+shell_install
