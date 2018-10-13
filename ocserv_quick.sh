8083143ff8bf155bbbe478925f03ca547e66d486
chendong12 <43974374+chendong12@users.noreply.github.com>
2018-10-13 23:13:39 +0800

    Update ocserv_quick.sh

--- ocserv_quick.sh
+++ ocserv_quick.sh
@@ -1,4 +1,5 @@
 #复制下面的所有代码，粘贴到你的服务器进行安装，你的服务器必须是Centos7
+#Copy all the code below, paste it into your server to install, your server must be Centos7
 sudo -i 
 yum install wget -y 
 wget https://raw.githubusercontent.com/chendong12/ocserv/master/install_script.sh 
