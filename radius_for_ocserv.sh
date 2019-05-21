7ba0ff419e2179503f7e2a32e93f34d06bcde934
chendong12 <43974374+chendong12@users.noreply.github.com>
2019-05-21 20:35:16 +0800

    Update radius_for_ocserv.sh

--- radius_for_ocserv.sh
+++ radius_for_ocserv.sh
@@ -4,7 +4,6 @@ function centos1_ntp(){
 	sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 	yum -y install ntp
 	service ntpd restart
-	cp -rf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 	cd /root
 	echo '0-59/10 * * * * /usr/sbin/ntpdate -u cn.pool.ntp.org' >> /tmp/crontab.back
 	crontab /tmp/crontab.back
