9a45cc08acb05653acf2bd69a4dce6632b18aeaa
chendong12 <43974374+chendong12@users.noreply.github.com>
2021-12-01 22:31:32 +0800

    Update change_ocserv_to_radius_auth.sh

--- radius/change_ocserv_to_radius_auth.sh
+++ radius/change_ocserv_to_radius_auth.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sed -i "s/#auth = \"radius\[config=\/etc\/radiusclient-ng\/radiusclient.conf,groupconfig=true\]\"/auth = \"radius\[config=\/etc\/radiusclient-ng\/radiusclient.conf,groupconfig=true\]\"/g" /etc/ocserv/ocserv.conf 
 sed -i "s/#acct = \"radius\[config=\/etc\/radiusclient-ng\/radiusclient.conf\]\"/acct = \"radius\[config=\/etc\/radiusclient-ng\/radiusclient.conf\]\"/g" /etc/ocserv/ocserv.conf
 sed -i "s/auth = \"plain\[passwd=\/etc\/ocserv\/ocpasswd\]\"/#auth = \"plain\[passwd=\/etc\/ocserv\/ocpasswd\]\"/g" /etc/ocserv/ocserv.conf
